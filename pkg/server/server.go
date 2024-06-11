e5009cf76db34c1a492000c783f6a96cb7526cd4
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2024-06-11 15:27:28 +0200

    linter fixing

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,8 +1,23 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package server
 
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -38,7 +53,7 @@ func (s *Server) Run(_ context.Context) error {
 		_, _ = w.Write([]byte("welcome"))
 	})
 
-	srv := &http.Server{Addr: s.Addr, Handler: r}
+	srv := &http.Server{Addr: s.Addr, Handler: r, ReadTimeout: 5 * time.Second}
 	s.server = srv
 
 	if s.Insecure {
